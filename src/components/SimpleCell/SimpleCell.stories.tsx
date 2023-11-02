aa2c881cc5c54fa8d7893f9221019b24a27719d9
andrejrebrov <andressc@mail.ru>
2023-11-02 18:22:24 +0300

    edit storyBook

--- src/components/SimpleCell/SimpleCell.stories.tsx
+++ src/components/SimpleCell/SimpleCell.stories.tsx
@@ -7,26 +7,33 @@ import {Meta, StoryObj} from "@storybook/react"
 
 const meta: Meta<typeof SimpleCell> = {
     component: SimpleCell,
+    tags: ['autodocs'],
+    parameters: {
+        docs: {
+            description: {
+                component: 'Компонент клетка пользователя'
+            },
+        },
+    },
 }
 
 
 export default meta
+
 type Story = StoryObj<typeof SimpleCell>;
 
+
 export const Cell: Story = {
-    tags: ['autodocs'],
     args: {
         image: "https://i.pinimg.com/736x/b0/2d/a7/b02da7cbdb25c5e1f1ae9f4e0f73903b.jpg",
         title: "Inna Morozova",
         subtitle: "12.05.1989"
     },
-}
-
-export const CellLegacy = () => {
-    return (
-        <SimpleCell image="https://i.pinimg.com/736x/b0/2d/a7/b02da7cbdb25c5e1f1ae9f4e0f73903b.jpg"
-                    title="Inna Morozova"
-                    subtitle="12.05.1989"
-        />
-    )
+    parameters: {
+        docs: {
+            description: {
+                story: 'Another description on the story, overriding the comments'
+            },
+        },
+    },
 }
--- src/components/SimpleCell/SimpleCell.tsx
+++ src/components/SimpleCell/SimpleCell.tsx
@@ -2,10 +2,28 @@ import React from "react"
 import * as C from "../Styled/Components"
 
 type SimpleCellPropsType = {
+    /**
+     * Whether there should be animation when the checkbox's state changes
+     *
+     * @default ergergerg
+     */
     image: string
+
+    /**
+     * Whether there should be animation when the checkbox's state changes
+     *
+     * @default ergergerg
+     */
     title?: string
+
+    /**
+     * Whether there should be animation when the checkbox's state changes
+     *
+     * @default ergergerg
+     */
     subtitle?: string
 }
+
 export const SimpleCell: React.FC<SimpleCellPropsType> = ({image, subtitle, title}) => {
     return (
         <C.FlexWrapper $gap={15} $align="center">

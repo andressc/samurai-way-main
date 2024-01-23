b903a17055ed8635cc043e9ca50ccf5e231cd6c7
andrejrebrov <andressc@mail.ru>
2024-01-23 15:21:11 +0300

    added local state

--- src/components/AboutUser/information/Information.styled.ts
+++ src/components/AboutUser/information/Information.styled.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components"
 
 export const InformationItem = styled.span`
-  span {
-    font-weight: 700;
-  }
+
 `
 
 export const Information = styled.div`
--- src/components/AboutUser/information/InformationItem.tsx
+++ src/components/AboutUser/information/InformationItem.tsx
@@ -1,14 +1,29 @@
-import React, {FC} from "react"
+import React, {Component} from "react"
 import * as S from "./Information.styled"
 
 type InformationItemPropsType = {
     title: string
     aboutMe: string
 }
-export const InformationItem: FC<InformationItemPropsType> = ({title, aboutMe}) => {
-    return (
-        <S.InformationItem>
-            <span>{title}:</span> {aboutMe}
-        </S.InformationItem>
-    )
+
+export class InformationItem extends Component<InformationItemPropsType> {
+
+    state = {isEdit: false}
+
+    onDoubleClickHandler() {
+        this.setState({isEdit: true})
+    }
+
+    onBlurHandler() {
+        this.setState({isEdit: false})
+    }
+
+    render() {
+        return (
+            <S.InformationItem>
+                <><span style={{fontWeight: 700}}>{this.props.title}:</span> {this.state.isEdit ?
+                    <input value={this.props.aboutMe} onBlur={this.onBlurHandler.bind(this)} autoFocus={true}/> : <span onDoubleClick={this.onDoubleClickHandler.bind(this)}>{this.props.aboutMe}</span>}</>
+            </S.InformationItem>
+        )
+    }
 }
